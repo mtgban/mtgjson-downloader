bc2f36f2384e0372a13d140e93cd0af3c4ac3668
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-08-16 15:26:33 +0200

    Import function code

---
+++ packages/default/downloader/downloader.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/hashicorp/go-cleanhttp"
+)
+
+func Main(args map[string]interface{}) map[string]interface{} {
+	URL := args["URL"].(string)
+
+	err := downloadAndUploadLatest(URL)
+	if err != nil {
+		return map[string]interface{}{
+			"statusCode": 500,
+			"body":       fmt.Sprintf("Error: %v", err),
+		}
+	}
+
+	return map[string]interface{}{
+		"statusCode": 200,
+		"body":       filepath.Base(URL) + " uploaded successfully",
+	}
+}
+
+func downloadAndUploadLatest(URL string) error {
+	filename := filepath.Base(URL)
+
+	log.Println("DOWNLOAD")
+
+	// Download to temp file
+	tmpFilePath := filepath.Join(os.TempDir(), filename)
+	outFile, err := os.Create(tmpFilePath)
+	if err != nil {
+		return fmt.Errorf("failed to create temp file: %w", err)
+	}
+	defer func() {
+		outFile.Close()
+		os.Remove(tmpFilePath)
+	}()
+
+	resp, err := cleanhttp.DefaultClient().Get(URL)
+	if err != nil {
+		return fmt.Errorf("failed to download file: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode/100 != 2 {
+		return fmt.Errorf("bad HTTP status: %s", resp.Status)
+	}
+
+	log.Println("COPY")
+
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to write to temp file: %w", err)
+	}
+
+	log.Println("UPLOAD")
+
+	// Rewind for reading
+	if _, err := outFile.Seek(0, io.SeekStart); err != nil {
+		return fmt.Errorf("failed to rewind temp file: %w", err)
+	}
+
+	// Setup Backblaze B2 S3 client
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-005"),
+		Endpoint:    aws.String(os.Getenv("B2_ENDPOINT")),
+		Credentials: credentials.NewStaticCredentials(os.Getenv("B2_KEY_ID"), os.Getenv("B2_APP_KEY"), ""),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create AWS session: %w", err)
+	}
+
+	s3Client := s3.New(sess)
+
+	// Upload latest file
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket:      aws.String(os.Getenv("B2_BUCKET")),
+		Key:         aws.String(filename),
+		Body:        outFile,
+		ACL:         aws.String("private"),
+		ContentType: aws.String("application/x-xz"),
+	})
+	if err != nil {
+		return fmt.Errorf("upload failed: %w", err)
+	}
+
+	log.Println("DONE")
+	return nil
+}
---
+++ packages/default/downloader/go.mod
@@ -0,0 +1,10 @@
+module mtgjsondownloaders/allprintings/packages/default/downloader
+
+go 1.20
+
+require github.com/aws/aws-sdk-go v1.55.8
+
+require (
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
